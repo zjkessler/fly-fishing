ce4fe7af1bcc998ee7a6bc128e2e6cd459bc5fe4
Zach Kessler <zjkessler@gmail.com>
2017-01-18 13:58:27 -0700

    database adjust

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-var express = require("express");
+var express = require( "express" );
 var app = express();
-var bodyParser = require("body-parser");
-var cors = require("cors")
-var mongoose = require("mongoose");
-var path = require("path");
-var config = require("./config");
-var logger = require("morgan");
-var expressJwt = require("express-jwt");
-var request = require('request');
+var bodyParser = require( "body-parser" );
+var cors = require( "cors" )
+var mongoose = require( "mongoose" );
+var path = require( "path" );
+var config = require( "./config" );
+var logger = require( "morgan" );
+var expressJwt = require( "express-jwt" );
+var request = require( 'request' );
 var port = process.env.PORT || 8000;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(logger("dev"));
+app.use( cors() );
+app.use( bodyParser.json() );
+app.use( express.static( path.join( __dirname, "public" ) ) );
+app.use( logger( "dev" ) );
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.database, function (err) {
-	if (err) throw err;
-	console.log("Successully connected to the database");
-});
+mongoose.connect( 'mongodb://heroku_hd3zdwkl:qd57v5tgc35pfbvrp3rlgaqjhc@ds117899.mlab.com:17899/heroku_hd3zdwkl', function ( err ) {
+    if ( err ) throw err;
+    console.log( "Successully connected to the database" );
+} );
 
-app.use("/api", expressJwt({
-	secret: config.secret
-}));
+app.use( "/api", expressJwt( {
+    secret: config.secret
+} ) );
 
-app.use("/api/feed/", require("./feed"));
-app.use("/scrape/", require("./scrape"));
-app.use("/api/profile", require("./routes/profileRoutes"));
-app.use("/api/report", require("./routes/reportRoutes"));
-app.use("/auth", require("./routes/authRoutes"));
+app.use( "/api/feed/", require( "./feed" ) );
+app.use( "/scrape/", require( "./scrape" ) );
+app.use( "/api/profile", require( "./routes/profileRoutes" ) );
+app.use( "/api/report", require( "./routes/reportRoutes" ) );
+app.use( "/auth", require( "./routes/authRoutes" ) );
 
 
-app.listen(port, function () {
-	console.log("Server is listening on port: " + port);
-});
+app.listen( port, function () {
+    console.log( "Server is listening on port: " + port );
+} );
